b7cadf4438dcff8548098a688dfdace37e5bf087
Jean-Baptiste Pasquier <pasquierjb@Jean-Baptistes-MacBook-Pro.local>
2018-03-16 10:10:40 +0100

    reproject with gpd

---
+++ GIS_utils.py
@@ -0,0 +1,151 @@
+def geojson_country_NE(country):
+    import requests
+    r = requests.get("https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson")
+    country_id=-1
+
+    data = r.json()
+    for i in range(0,len(data["features"])):
+        for j in data["features"][i]["properties"]:
+            if data["features"][i]["properties"][j]==country:
+                country_id=i
+                break
+        else:
+            continue
+        break
+
+    geojson={
+      "type": "Feature",
+      "properties": {},
+      "geometry": {}
+    }
+
+    if country_id==-1:
+        print("No Boundaries available for this request")
+    else:
+        geojson["geometry"]=data["features"][country_id]["geometry"]
+        return geojson
+
+
+def geojson_country_OSM(country_ISO):
+    import requests
+    from zipfile import ZipFile
+    import io
+    import json
+    params = (
+    ('cliVersion', '1.0'),
+    ('cliKey', 'f7249a6c-5e0c-4834-823c-eef0167aebac'),
+    ('exportFormat', 'json'),
+    ('exportLayout', 'levels'),
+    ('exportAreas', 'land'),
+    ('union', 'false'),
+    ('selected', country_ISO))
+
+    r = requests.get('https://wambachers-osm.website/boundaries/exportBoundaries', params=params)
+
+    if r.status_code == requests.codes.ok:
+        zipfile = ZipFile(io.BytesIO(r.content))
+        zip_names = zipfile.namelist()
+
+        data = zipfile.open(zip_names[0]).read()
+        data = json.loads(data.decode("utf-8"))
+
+        geojson={
+          "type": "Feature",
+          "properties": {},
+          "geometry": {}
+        }
+        geojson["geometry"]=data["features"][0]["geometry"]
+
+        return geojson
+    else:
+        print("No Boundaries available for this request")
+
+def reproject_geojson_gdal(geojson,dst_crs,src_crs=4326,):
+    from osgeo import ogr, osr
+    import json
+    source = osr.SpatialReference()
+    source.ImportFromEPSG(src_crs)
+    target = osr.SpatialReference()
+    target.ImportFromEPSG(dst_crs)
+    transform = osr.CoordinateTransformation(source, target)
+    polygon = ogr.CreateGeometryFromJson("""{}""".format(geojson["geometry"]))
+    polygon.Transform(transform)
+    geojson_reproj={
+      "type": "Feature",
+      "properties": {},
+      "geometry": {}
+    }
+    geojson_reproj["geometry"]=json.loads(polygon.ExportToJson())
+    return geojson_reproj
+
+def reproject_geojson_gpd(geojson,dst_crs,src_crs=4326):
+    import geopandas as gpd
+    from shapely.geometry import shape
+    import json
+    goodshape=shape(geojson["geometry"])
+    gdf = gpd.GeoSeries(goodshape,crs={'init': 'epsg:{}'.format(src_crs)})
+    gdf = gdf.to_crs({'init': 'epsg:{}'.format(dst_crs)})
+    geojson_reproj={
+      "type": "Feature",
+      "properties": {},
+      "geometry": {}
+    }
+    geojson_reproj["geometry"]=json.loads(gdf.to_json())["features"][0]["geometry"]
+    return geojson_reproj
+
+def rasterize_geojson(geojson,resolution,dst_raster,src_crs):
+    from rasterio.features import bounds as calculate_bounds
+    from math import ceil
+    from affine import Affine
+    from rasterio.features import rasterize
+    from rasterio.io import MemoryFile
+    import rasterio
+    bounds = calculate_bounds(geojson)
+    res = (resolution,resolution)
+    geometries = ((geojson["geometry"], 1), )
+    params = {
+    'count': 1,
+    'crs':'EPSG:{}'.format(src_crs),
+    'width': max(int(ceil((bounds[2] - bounds[0]) / float(res[0]))), 1),
+    'height': max(int(ceil((bounds[3] - bounds[1]) /float(res[1]))), 1),
+    'driver': 'GTiff',
+    'transform': Affine(res[0], 0, bounds[0], 0, -res[1], bounds[3]),
+    'nodata': 0,
+    'dtype': 'uint8'
+    }
+
+    output = rasterize(
+                geometries,
+                out_shape=(params['height'], params['width']),
+                transform=params['transform'])
+
+    with rasterio.open(dst_raster,'w',**params) as dst:
+            dst.write(output, indexes=1)
+    return dst_raster, output
+
+def reproject_raster(src_raster,dst_raster,dst_crs):
+    import numpy as np
+    import rasterio
+    from rasterio.warp import calculate_default_transform, reproject, Resampling
+    dst_crs = 'EPSG:{}'.format(dst_crs)
+    with rasterio.open(src_raster) as src:
+        transform, width, height = calculate_default_transform(src.crs, dst_crs, src.width, src.height, *src.bounds)
+        kwargs = src.meta.copy()
+        kwargs.update({
+            'crs': dst_crs,
+            'transform': transform,
+            'width': width,
+            'height': height
+        })
+
+        with rasterio.open(dst_raster, 'w', **kwargs) as dst:
+            for i in range(1, src.count + 1):
+                reproject(
+                    source=rasterio.band(src, i),
+                    destination=rasterio.band(dst, i),
+                    src_transform=src.transform,
+                    src_crs=src.crs,
+                    dst_transform=transform,
+                    dst_crs=dst_crs,
+                    resampling=Resampling.nearest)
+        return dst_raster
--- countrytoraster.py
+++ countrytoraster.py
@@ -1,21 +1,11 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort, send_file, make_response
-import rasterio
-import requests
-from shapely.geometry import shape, mapping
-from rasterio.features import bounds as calculate_bounds
-from math import ceil
-from rasterio.features import rasterize
-from affine import Affine
-import json
-from osgeo import ogr, osr
+from GIS_utils import geojson_country_NE, geojson_country_OSM, reproject_geojson_gdal, \
+    reproject_geojson_gpd, rasterize_geojson, reproject_raster
+from flask import Flask, request, send_file, make_response, render_template
+import os
 import matplotlib
-matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import io
-from PIL import Image
-from rasterio.io import MemoryFile
-from zipfile import ZipFile
-import os 
+matplotlib.use("Agg")
 
 
 app = Flask(__name__)
@@ -39,111 +29,33 @@ def home():
     return render_template("index.html")
 
 
-
 @app.route("/convert",methods=["POST"])
 def countrytoraster():
-    country=request.form["country"]
-    resolution=float(request.form["gsize"])
-    projection=int(request.form["projection"])
-    api=int(request.form["api_field"])
+    country = request.form["country"]
+    resolution = float(request.form["gsize"])
+    projection = int(request.form["projection"])
+    api = int(request.form["api_field"])
+    name = "{}_{}_{}_{}".format(country, resolution, projection, api)
 
-    print(request.form)
+    if api == 1:
+        geojson=geojson_country_NE(country)
+    elif api == 2:
+        geojson = geojson_country_OSM(country)
 
-    name="{}_{}_{}_{}".format(country, resolution, projection, api)
+    if geojson==None:
+        return "No Boundaries available for this request"
 
-    #template geojson in the correct format
-    geojson_template_polygon={
-      "type": "Feature",
-      "properties": {},
-      "geometry": {}
-    }
+    geojson_reproj = reproject_geojson_gpd(geojson,src_crs=4326,dst_crs=projection)
 
-    #Get a geojson from the openstreetmap api
-    #url="http://nominatim.openstreetmap.org/search?country={}&polygon_geojson=1&format=json".format(country)
-    geojson=geojson_template_polygon
+    raster_path, output = rasterize_geojson(geojson_reproj,resolution,dst_raster="/tmp/raster.tif",src_crs=projection)
 
-    if api==1:
-        ##1st API: Natural Earth
-        r = requests.get("https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson")
-        data = r.json()
-        for i in range(0,len(data["features"])):
-            for j in data["features"][i]["properties"]:
-                if data["features"][i]["properties"][j]==country:
-                    country_id=i
-                    break
-            else:
-                continue
-            break
-        geojson["geometry"]["coordinates"]=data["features"][country_id]["geometry"]["coordinates"]
-        geojson["geometry"]["type"]=data["features"][country_id]["geometry"]["type"]
-
-
-    elif api==2:
-        ## 2nd API: Open Street Map
-        params = (
-        ('cliVersion', '1.0'),
-        ('cliKey', 'f7249a6c-5e0c-4834-823c-eef0167aebac'),
-        ('exportFormat', 'json'),
-        ('exportLayout', 'levels'),
-        ('exportAreas', 'land'),
-        ('union', 'false'),
-        ('selected', country))
-
-        r = requests.get('https://wambachers-osm.website/boundaries/exportBoundaries', params=params)
-
-        zipfile = ZipFile(io.BytesIO(r.content))
-        zip_names = zipfile.namelist()
-
-        data = zipfile.open(zip_names[0]).read()
-        data = json.loads(data.decode("utf-8"))
-
-        geojson["geometry"]["coordinates"]=data["features"][0]["geometry"]["coordinates"]
-        geojson["geometry"]["type"]=data["features"][0]["geometry"]["type"]
-
-
-    #Reproject the original geojson with GDAL osr and ogr
-    source = osr.SpatialReference()
-    source.ImportFromEPSG(4326)
-    target = osr.SpatialReference()
-    target.ImportFromEPSG(projection)
-    transform = osr.CoordinateTransformation(source, target)
-    polygon = ogr.CreateGeometryFromJson("""{}""".format(geojson["geometry"]))
-    polygon.Transform(transform)
-    geojson_reproj=json.loads(polygon.ExportToJson())
-
-    #Rasterize the polygon with rasterio
-    bounds = calculate_bounds(geojson_reproj)
-    res = (resolution,resolution)
-    geometries = ((geojson_reproj, 1), )
-    params = {
-    'count': 1,
-    'crs':'EPSG:{}'.format(projection),
-    'width': max(int(ceil((bounds[2] - bounds[0]) / float(res[0]))), 1),
-    'height': max(int(ceil((bounds[3] - bounds[1]) /float(res[1]))), 1),
-    'driver': 'GTiff',
-    'transform': Affine(res[0], 0, bounds[0], 0, -res[1], bounds[3]),
-    'nodata': 0,
-    'dtype': 'uint8'
-    }
-
-    output = rasterize(
-                geometries,
-                out_shape=(params['height'], params['width']),
-                transform=params['transform'])
-
-    with MemoryFile() as memfile:
-        with memfile.open(**params) as dst:
-                dst.write(output, indexes=1)
-        with open('/tmp/file.tif', 'wb') as g:
-            memfile.seek(0)
-            g.write(memfile.read())
-            if request.form["action"] == "download":
-                return send_file('/tmp/file.tif',
-                                 mimetype='image/tiff',
-                                 as_attachment=True,
-                                 attachment_filename=name+".tif")
-            elif request.form["action"] == "preview":
-                return output_showcase(output)
+    if request.form["action"] == "download":
+        return send_file(raster_path,
+                         mimetype='image/tiff',
+                         as_attachment=True,
+                         attachment_filename=name+".tif")
+    elif request.form["action"] == "preview":
+        return output_showcase(output)
 
 
 if __name__ == "__main__":
